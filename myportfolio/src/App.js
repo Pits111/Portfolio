4d6356246b1b4a3337ea4bf970d9bc328a989028
Isabel <isabel.mehlmann@gmail.com>
2021-05-13 13:03:24 +0200

    added wave background

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>This is the start</p>
+      <p>please show an image!</p>
     </div>
   );
 }
