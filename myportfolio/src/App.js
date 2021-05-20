a36a470b5b03ed28467588b45563cb238597ac5c
Isabel <isabel.mehlmann@gmail.com>
2021-05-20 09:05:47 +0200

    added portfolio cards

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -4,19 +4,45 @@ import Navbar from './components/Navbar';
 import Contact from './components/Contact';
 import Banner from './components/Banner';
 import Home from './components/Intro';
+import CardsList from './components/BooksList';
+
+
+const cards = [
+  {
+      title: "Tech Skills",
+      imgUrl: "https://thumbs2.imgbox.com/4f/63/aEakTFmS_t.png",
+      body: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet"
+  },
+  {
+      title: "Soft Skills",
+      imgUrl: "https://thumbs2.imgbox.com/4f/63/aEakTFmS_t.png",
+      body: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet"
+  },
+  {
+      title: "Nobody Kills",
+      imgUrl:"https://thumbs2.imgbox.com/4f/63/aEakTFmS_t.png",
+      body:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet"
+  },
+  {
+      title: "Everyones Skills",
+      imgUrl: "https://thumbs2.imgbox.com/4f/63/aEakTFmS_t.png",
+      body: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet"
+  },
+]
+
+
 
 function App() {
-
-  const myStyle={
-  }
-
   return (
     <div className="App">
           <Navbar/>
+          
           <Banner/>
           <Switch>
           <Route path='/Home' component={Home} />
-          <Route path="/Portfolio" component={Home} />
+          <Route path="/Portfolio">
+          <CardsList cardsInfo={cards} />
+          </Route> 
           <Route path="/Contact" component={Contact}/>
           </Switch>
     </div>
