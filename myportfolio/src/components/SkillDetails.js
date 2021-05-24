163e9e6e64b513ac74d47194739f736ba5b824e6
Isabel <isabel.mehlmann@gmail.com>
2021-05-24 16:30:48 +0200

    added links to images and deleted buttons

--- myportfolio/src/components/SkillDetails.js
+++ myportfolio/src/components/SkillDetails.js
@@ -17,7 +17,7 @@ const SkillDetails = ({ routeProps, title }) => {
             subthree: "To See More About > WILD CODE SCHOOL < click on the image below ",
             imgone: "https://i.postimg.cc/dtntBqs0/Tech-Stack-Hochaufloesung.png",
             imgtwo: "https://i.postimg.cc/4ycvf02G/WCS.png",
-            button: "https://www.youtube.com/watch?v=VIr0YKX5Fbk",
+            Link: "https://www.youtube.com/watch?v=AD-IUNA4YZw",
             id: 5,
         },
         {
@@ -26,10 +26,10 @@ const SkillDetails = ({ routeProps, title }) => {
             paraone: "Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AI knowledge has made the evaluation process rather uncertain.Business executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AI knowledge has made the evaluation process rather uncertain.",
             subtwo: "New title",
             paratwo: "Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AI knowledge has made the evaluation process rather uncertain.",
-            subthree: "To see more about > KIBALOU < click on the image below",
+            subthree: "To see more about KIBALOU click on the image below",
             imgone: "https://i.postimg.cc/jdgjVSzW/Soft-Skills-Hochaufloesung.png",
             imgtwo: "https://i.postimg.cc/6Ts7M50J/Kibalou.png",
-            button: "https://www.youtube.com/watch?v=VIr0YKX5Fbk",
+            Link: "https://battyrabbit.com/portfolio/kibalou/",
             id: 6, 
         },
         {
@@ -38,10 +38,10 @@ const SkillDetails = ({ routeProps, title }) => {
             paraone: "Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AI knowledge has made the evaluation process rather uncertain.Business executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AI knowledge has made the evaluation process rather uncertain.",
             subtwo: "newtitle",
             paratwo: "Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI.Business executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AI knowledge has made the evaluation process rather uncertain.",
-            subthree: "To read the the whole > WHITEPAPER < click on the image below",
+            subthree: "To read the WHITEPAPER click on the image below",
             imgone: "https://i.postimg.cc/rshqLft6/Chess-Hochaufloesung.png",
             imgtwo: "https://i.postimg.cc/hv8J3R3X/TKCover.png",
-            button: "https://www.youtube.com/watch?v=VIr0YKX5Fbk",
+            Link: "https://www.tk.de/resource/blob/2057560/79350ea011f613307781908115772d6c/position-digitale-gesundheit-data.pdf",
             id: 7, 
             
         },
@@ -51,10 +51,10 @@ const SkillDetails = ({ routeProps, title }) => {
             paraone: "Business executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AIBusiness executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AIBusiness executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AIBusiness executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AI knowledge has made the evaluation process rather uncertain.",
             subtwo: "New title",
             paratwo: "Business executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AIBusiness executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AIBusiness executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AIBusiness executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AI knowledge has made the evaluation process rather uncertain.",
-            subthree: "To see the > AI-Video <  in german, click on the image below.",
+            subthree: "To see the AI-Video click on the image below.",
             imgone: "https://i.postimg.cc/8kdFYGKW/Ligth-Bulb-Hochaufloesung.png",
             imgtwo: "https://i.postimg.cc/DZHdKhg5/TKVideo-Two.png",
-            button: "https://www.youtube.com/watch?v=VIr0YKX5Fbk",
+            Link: "https://www.youtube.com/watch?v=VIr0YKX5Fbk",
             id: 8, 
         },
     ] 
@@ -86,13 +86,17 @@ const SkillDetails = ({ routeProps, title }) => {
                         <h3>{foundMatch.subthree}</h3>
                     </div>
                     <div className="imgtwo-container">
+                        <a href={foundMatch.Link}>
                          <img src={foundMatch.imgtwo} alt='picture' />
+                        </a>
                     </div>
                    
                     <div className="skill-btn">
+                        <Link to = {`/portfolio`}>
                         <button>
                             <a>Go back</a>
                         </button>
+                        </Link>
                        
                     </div>
                 </div>
