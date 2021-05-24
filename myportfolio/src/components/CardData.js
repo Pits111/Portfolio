bf62df3ab8d86f9b8e36d8d4ed214ac2d80a90cf
Isabel <isabel.mehlmann@gmail.com>
2021-05-24 12:10:29 +0200

    adding router props and match function

--- myportfolio/src/components/CardData.js
+++ myportfolio/src/components/CardData.js
@@ -1,6 +1,4 @@
 
-
-
 const cards = [
     {
         title: "Research",
--- myportfolio/src/components/SkillDetails.js
+++ myportfolio/src/components/SkillDetails.js
@@ -1,19 +1,41 @@
 
 const SkillDetails = ({ routeProps }) => {
+    console.log(routeProps);
     
-    const SkillInfo = [
+    const skillContent = [
         {
-        title: "loremipsum",
-        info: "some text here",
-        links: "for more information go here",
-        placehoder: " I do not know what else",
-        id: "2", 
-        }
+            title: "Tech Skills",
+            text: "Business executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AI knowledge has made the evaluation process rather uncertain.",
+            source: "Link",
+            id: 5, 
+        },
+        {
+            title: "Soft Skills",
+            text: "Business executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AI knowledge has made the evaluation process rather uncertain.",
+            source: "Link",
+            id: 6, 
+        },
+        {
+            title: "Strategy",
+            text: "Business executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AI knowledge has made the evaluation process rather uncertain.",
+            source: "Link",
+            id: 7, 
+        },
+        {
+            title: "Creativity",
+            text: "Business executives have notoriously struggled to assess the business value of AI. They understand the potential value of it, but the general lack of institutional AI knowledge has made the evaluation process rather uncertain.",
+            source: "Link",
+            id: 8, 
+        },
     ] 
-    
+
+    const foundMatch = skillContent.find((skillCategory) => skillCategory.title === routeProps.match.params.skilltitle)
+        console.log(foundMatch);
     return (
         <div>
-            <p>Why does it not work here?</p>
+            <h1>{foundMatch.title}</h1>
+            <p>{foundMatch.text}</p>
+            <p>{foundMatch.source}</p>
         </div>
     );
 };
